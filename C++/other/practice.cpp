25d793e98073c3c3f2ba10f57a9b3edb4fd45a6b
Xithrius <Xithrius@gmail.com>
2018-11-14 12:02:55 -0800

    setting up website, c++.

--- C++/other/practice.cpp
+++ C++/other/practice.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 using namespace std;
 
-static main()
-
-int main()
-{
-     cout << sum ( 3, 4) << endl;
-     int result = sum(1, 3, 5);
-     cout << result << endl;
+void dashedLine(int a) {
+    if (a > 0) {
+        while (a > 0) {
+            cout << "-";
+            --a;
+        }
+    }
 }
 
-int sum(int a, int b, int c) {
-  d = a + b + c;
-  return d;
+int main() {
+  dashedLine(2);
 }
