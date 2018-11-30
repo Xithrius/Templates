4ee926c10585b5904c353ba8062c3307eda0aa33
Xithrius <Xithrius@gmail.com>
2018-11-30 10:52:11 -0800

    .gitignore main

--- C++/assignments/Assignment7/mainCopy.cpp
+++ C++/assignments/Assignment7/mainCopy.cpp
@@ -1,92 +1,138 @@
+/*
+Program purpose:
+Get answers from student, make them answer again
+if they didn't answer in one of the options,
+and after the test is done they get which Questions
+they got wrong, and how many incorrect and correct
+they got
+*/
 #include <iostream>
-#include <iomanip>
-#include <ctype.h>
 
 using namespace std;
 
-void input(char[], int);
-void checkAnswers(char[], char[], int &, int &, int);
-
-
+void input(char [], int);
+void checkAnswers(char [], char [], int &, int &, int);
 
 int main()
 {
-   char questions[20];
-   const int Total = 20;
-   char rightAnswers[] = {'A', 'D', 'B', 'B', 'C',
-                          'B', 'A', 'B', 'C', 'D',
-                          'A', 'C', 'D', 'B', 'D',
-                          'C', 'C', 'A', 'D','B'};
-
-   cout << "Please enter the student's answers " << endl;
-   cout << "Press Enter after typing each answer." << endl;
-   cout << "Please enter a valid choice" << endl;
-   cout << "A, B, C, D or a, b, c, d" << endl;
-
-    input(ques, arrTotal);
-    checkAnswers(ques, rightAns, correct, incorrect, arrTotal);
-
-    if ( correct <= 12)
-    {
-        cout << "The student has failed" << endl;
+    int array_size = 20;
+    int correct, incorrect;
+    char rightAnswers[array_size] = {'A', 'D', 'B', 'B', 'C',
+                                     'B', 'A', 'B', 'C', 'D',
+                                     'A', 'C', 'D', 'B', 'D',
+                                     'C', 'C', 'A', 'D', 'B'};
+    char userAnswers[array_size];
+    cout << "Please enter the student's answers for each of the questions.";
+    cout << endl;
+    cout << "Press Enter after typing each answer." << endl;
+    cout << "Please enter only an A, B, C, D or ";
+    cout << "a, b, c, d for each question." << endl;
+    input(userAnswers, array_size);
+    checkAnswers(userAnswers, rightAnswers, correct,
+                 incorrect, array_size);
+    if (correct <= 14) {
+        cout << "The student failed the exam" << endl;
     }
-    else
-    {
-        cout << "The student has passed" << endl;
+    else if (correct >= 15) {
+        cout << "The student passed the exam" << endl;
     }
+    cout << "Correct answers: " << correct << endl;
+    cout << "Incorrect answers: " << incorrect << endl;
 
-    cout << "Correct Answers: " << correct << endl;
-    cout << "Incorrect Answers: " << incorrect << endl;
-
- }
-
- void input (char question[], int arrT)
- {
-
-     for ( int i = 1; i <= 20; i++ )
-     {
-         cout << "Question " << i << ": " << endl;
-         cin >> question[i];
-
-         while (question[i] != 'A' &&
-                question[i] != 'B' &&
-                question[i] != 'C' &&
-                question[i] != 'D' &&
-                question[i] != 'a' &&
-                question[i] != 'b' &&
-                question[i] != 'c' &&
-                question[i] != 'd')
-     {
-
-         cout << "Use only an A, B, C, D or a, b, c, d!" << endl;
-         cout << "Please try again." << endl;
-         cin >> question[i];
-
-     }
-         putchar(toupper(question[i]));
-     }
-
-
+    return 0;
 }
 
-void checkAnswers(char question[], char answer[], int &right, int &wrong, int arrT)
+void input(char userAnswers[], int z)
 {
-    int counter;
-    cout << "Question that were answered incorrectly:" << endl;
-
-   for ( int i = 0; i < arrT; i++)
-   {
-    if (question[i] == answer[i])
-    {
-        right++;
+    int i = 0;
+    while (i < z) {
+        cout << "Question " << i + 1 << ": ";
+        cin >> userAnswers[i];
+        if (userAnswers[i] != 'A' &&
+            userAnswers[i] != 'B' &&
+            userAnswers[i] != 'C' &&
+            userAnswers[i] != 'D' &&
+            userAnswers[i] != 'a' &&
+            userAnswers[i] != 'b' &&
+            userAnswers[i] != 'c' &&
+            userAnswers[i] != 'd') {
+                cout << "Use only an A, B, C, D or a, b, c, d!" << endl;
+                cout << "Please try again." << endl;
+            }
+        else
+            i++;
     }
-    else
-    {
-        wrong++;
-        counter = i +1;
-        cout << counter << endl;
-    }
-   }
-
-
 }
+
+void checkAnswers(char userAnswers[], char rightAnswers[], int &correct,
+                  int &incorrect, int array_size)
+{
+    correct = 0;
+    incorrect = 0;
+    int i = 0;
+    cout << "Questions that were answered incorrectly: " << endl;
+    while (i < array_size) {
+        if (userAnswers[i] == rightAnswers[i]) {
+            ++correct;
+            ++i;
+        }
+        else
+            ++incorrect;
+            cout << i << endl;
+            ++i;
+    }
+}
+/*
+My run is incorrect, working on a fix
+Please enter the student's answers for each of the questions.
+Press Enter after typing each answer.
+Please enter only an A, B, C, D or a, b, c, d for each question.
+Question 1: A
+Question 2: C
+Question 3: B
+Question 4: B
+Question 5: a
+Question 6: d
+Question 7: d
+Question 8: d
+Question 9: a
+Question 10: b
+Question 11: v
+Use only an A, B, C, D or a, b, c, d!
+Please try again.
+Question 11: a
+Question 12: F
+Use only an A, B, C, D or a, b, c, d!
+Please try again.
+Question 12: d
+Question 13: a
+Question 14: A
+Question 15: C
+Question 16: B
+Question 17: A
+Question 18: D
+Question 19: B
+Question 20: C
+Questions that were answered incorrectly:
+1
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+The student failed the exam
+Correct answers: 2
+Incorrect answers: 16
+*/
