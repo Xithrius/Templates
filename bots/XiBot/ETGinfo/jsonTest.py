ee3bf7602308e8c3e643affbe976c7207d7274e7
Xithrius <Xithrius@gmail.com>
2018-12-12 09:43:05 -0800

    opening .json files, less hardcode

--- bots/XiBot/ETGinfo/jsonTest.py
+++ bots/XiBot/ETGinfo/jsonTest.py
@@ -1,5 +1,20 @@
 import json
+import os
 
-with open('D:/github_repositories/CODING/bots/XiBot/ETGinfo/Gungeoneers.json', 'r') as f:
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+with open(dir_path + '/gungeoneers.json', 'r') as f:
     gungeoneers = json.load(f)
-print(gungeoneers["The Convict"]["Starting Weapons"])
+print(gungeoneers["The Convict"]["Starting Weapons"][0:])
+
+
+
+@comms.command(pass_context=True)
+async def etg(self, ctx, searchType, name):
+    if searchType in ['gungeoneers', 'items', 'guns', 'bosses', 'CotG']:
+        with open(f"pathing() + /{searchType}.json", 'r') as f:
+            gungeoneer = json.load(f)
+        embed=discord.Embed(title=f"{gungeoneer}", description="This is the place where you get help", color=300000, timestamp=datetime.datetime.now())
+    else:
+        ctx.send(f"{ctx.message.author.mention} {searchType} only Gungeoneers, Items, Guns, Bosses, and the Cult of the Gundead exist")
