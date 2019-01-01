3c0d79718e814b8a54515322ccd91ec2a85f30d6
Xithrius <Xithrius@gmail.com>
2018-12-31 19:17:25 -0800

    Moving many files (391)

--- bots/bot1/indexWOtoken.js
+++
@@ -1,7 +0,0 @@
-const commando = require('discord.js-commando');
-const bot = new Discord.Client();
-
-bot.registry.registerGroup('random', 'Random');
-bot.registry.registerCommandsIn(__dirname + "/commands");
-
-bot.login('');
