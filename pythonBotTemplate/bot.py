a3c93771db9145121174f3fa75d2367e55838fe1
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-02-08 12:12:51 -0800

    Finished inputTesting function, will help for checking options while asking for inputs.

--- pythonBotTemplate/bot.py
+++ pythonBotTemplate/bot.py
@@ -1,22 +1,55 @@
+#-----------------------------------------------------------------------------#
+# Libraries
+#-----------------------------------------------------------------------------
 import discord
 from discord.ext import commands as comms
 import os
 import json
 import platform
 import sys
+#/////////////////////////////////////////////////////////////////////////////#
 
-
+#-----------------------------------------------------------------------------#
 # Different paths for different operating systems
+#-----------------------------------------------------------------------------
 def pathing(object):
     if platform.system() == "Windows":
         return f"{os.path.dirname(os.path.realpath(__file__))}\\{object}"
     elif platform.system() == "Linux":
         return f"{os.path.dirname(os.path.realpath(__file__))}/{object}"
+#/////////////////////////////////////////////////////////////////////////////#
 
+#-----------------------------------------------------------------------------#
+# Input checking for all the needs
+#-----------------------------------------------------------------------------
+def inputTest(string):
+    check = True
+    while check:
+        for i in range(len(string)):
+            if string[i] == '[':
+                forwardBracket = i + 1
+            elif string[i] == ']':
+                backwardBracket = i
+        options = (string[forwardBracket:backwardBracket]).split("/")
+        print(string, end='', flush=True)
+        In = input(" ")
+        if In in options:
+            return In
+            check = False
+        else:
+            print(f"Input does not match options of {', '.join(str(y) for y in options)}")
+#/////////////////////////////////////////////////////////////////////////////#
 
+#-----------------------------------------------------------------------------#
+#
+#-----------------------------------------------------------------------------
 class MainCog():
     def __init__(self, bot):
         self.bot = bot
+
+    @comms.command()
+    async def help(self, ctx):
+        ctx.send(embed=embed)
 
     @comms.command()
     async def exit(self, ctx):
@@ -25,20 +58,26 @@ class MainCog():
             await self.bot.logout()
         else:
             await ctx.send(f"{ctx.author.mention} you cannot do this")
+#/////////////////////////////////////////////////////////////////////////////#
 
-
+#-----------------------------------------------------------------------------#
+#
+#-----------------------------------------------------------------------------
 def initErrorCheck():
     try:
         with open(pathing('login.json'), "r") as f:
             login = json.load(f)
             return [login["discord"]["owner_id"], login["discord"]["bot_token"]]
     except FileNotFoundError:
-        with open(pathing('login.json'), "w") as f:
-            errorPrompt("")
+        errorMessage("The file login.json cannot be found.", sys.exc_info())
+        with open(pathing('login.json'), "r") as f:
     except IndexError:
-        errorMessage("The file login.json has been modified manually and cannot be read.", sys.exc_info())
+        errorMessage("The file login.json has been modified and cannot be read.", sys.exc_info())
+#/////////////////////////////////////////////////////////////////////////////#
 
-
+#-----------------------------------------------------------------------------#
+#
+#-----------------------------------------------------------------------------
 def errorPrompt(string, type=None):
     if type != None:
         x = f"#{'/' * len(string)}#"
@@ -46,17 +85,28 @@ def errorPrompt(string, type=None):
     else:
         x = f"#{'/' * len(string)}#"
         print(f"{x}\n{string}\n{x}")
+#/////////////////////////////////////////////////////////////////////////////#
 
+#-----------------------------------------------------------------------------#
+#
+#-----------------------------------------------------------------------------
 
 class BotClient(comms.Bot):
     async def on_ready(self):
         print(f"Logging in as {bot.user}")
         print(f"{bot.user} ID: {bot.user.id}")
         print("Awaiting...")
+#/////////////////////////////////////////////////////////////////////////////#
 
-
+#-----------------------------------------------------------------------------#
+#
+#-----------------------------------------------------------------------------
 logins = initErrorCheck()
-bot = BotClient(command_prefix = "$", owner_id = logins[0])
-bot.remove_command("help")
-bot.add_cog(MainCog(bot))
-bot.run(logins[1])
+try:
+    bot = BotClient(command_prefix = "$", owner_id = logins[0])
+    bot.remove_command("help")
+    bot.add_cog(MainCog(bot))
+    bot.run(logins[1])
+except KeyboardInterrupt:
+    bot.logout()
+#/////////////////////////////////////////////////////////////////////////////#
