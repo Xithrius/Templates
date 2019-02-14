72b8d389a4e6568f7956d777060cb85e7deadf81
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-02-14 00:05:19 -0800

    Changing the format of the bot

--- pythonBotTemplate/bot.py
+++ pythonBotTemplate/bot.py
@@ -1,67 +1,39 @@
 #-----------------------------------------------------------------------------#
 # Libraries
 #-----------------------------------------------------------------------------
-import discord
 from discord.ext import commands as comms
-import os
-import json
-import platform
+import discord
 import sys
+import os
+import platform
 #/////////////////////////////////////////////////////////////////////////////#
+
+
+#=============================================================================#
+#=============================================================================#
+# Essential functions
+#=============================================================================#
+#=============================================================================#
+
 
 #-----------------------------------------------------------------------------#
 # Different paths for different operating systems
+#####################
+# os, platform
 #-----------------------------------------------------------------------------
 def pathing(object):
     if platform.system() == "Windows":
         return f"{os.path.dirname(os.path.realpath(__file__))}\\{object}"
     elif platform.system() == "Linux":
         return f"{os.path.dirname(os.path.realpath(__file__))}/{object}"
+    else:
+        errorPrompt("Platform is not supported. Contact my creator.")
 #/////////////////////////////////////////////////////////////////////////////#
 
 #-----------------------------------------------------------------------------#
-# Main commands and events
-#-----------------------------------------------------------------------------
-class MainCog():
-    def __init__(self, bot):
-        self.bot = bot
-
-    @comms.command()
-    async def help(self, ctx):
-        ctx.send(embed=embed)
-
-    @comms.command()
-    async def exit(self, ctx):
-        if await ctx.bot.is_owner(ctx.author):
-            print("Exiting...")
-            await self.bot.logout()
-        else:
-            await ctx.send(f"{ctx.author.mention} you cannot do this")
-#/////////////////////////////////////////////////////////////////////////////#
-
-#-----------------------------------------------------------------------------#
-# Checking files
-#-----------------------------------------------------------------------------
-def initErrorCheck():
-    try:
-        with open(pathing('login.json'), "r") as f:
-            login = json.load(f)
-            return [login["discord"]["owner_id"], login["discord"]["bot_token"]]
-    except FileNotFoundError:
-        errorMessage("The file login.json cannot be found.", sys.exc_info())
-        with open(pathing('login.json'), "r+") as f:
-            owner_id = input("Input discord user owner for owner_id: ")
-            bot_token = input("Input discord bot token for bot_token: ")
-            tokens = {
-            "discord": [owner_id, bot_token]
-            }
-            json.dump(logins, f)
-    except IndexError:
-        errorMessage("The file login.json has been modified and cannot be read.", sys.exc_info())
-#/////////////////////////////////////////////////////////////////////////////#
-
-#-----------------------------------------------------------------------------#
-# When there is an error, this will be called and shown to console.
+# Custom error printing to the console
+######################################
+# Python built in libraries
 #-----------------------------------------------------------------------------
 def errorPrompt(string, type=None):
     if type != None:
@@ -73,7 +45,9 @@ def errorPrompt(string, type=None):
 #/////////////////////////////////////////////////////////////////////////////#
 
 #-----------------------------------------------------------------------------#
-# Input checking for all the needs
+# Custom input looping from console
+###################################
+# Python built in libraries
 #-----------------------------------------------------------------------------
 def inputTest(string):
     check = True
@@ -93,19 +67,78 @@ def inputTest(string):
             print(f"Input does not match options of {', '.join(str(y) for y in options)}")
 #/////////////////////////////////////////////////////////////////////////////#
 
+
+#=============================================================================#
+#=============================================================================#
+# Cogs
+#=============================================================================#
+#=============================================================================#
+
+
 #-----------------------------------------------------------------------------#
-# The event tells by console when the bot is ready to be used.
+# Main cog for commands and events
+#####################
+# discord.ext.commands as comms
+#-----------------------------------------------------------------------------
+class MainCog():
+    def __init__(self, bot):
+        self.bot = bot
+
+    @comms.command()
+    async def help(self, ctx):
+        ctx.send(embed=embed)
+
+    @comms.command()
+    async def exit(self, ctx):
+        if await ctx.bot.is_owner(ctx.author):
+            print("Exiting...")
+            await self.bot.logout()
+        else:
+            await ctx.send(f"{ctx.author.mention} you cannot do this")
+#/////////////////////////////////////////////////////////////////////////////#
+
+#-----------------------------------------------------------------------------#
+# Checking to see if credentials exist to start bot
+###################################################
+# json
+#-----------------------------------------------------------------------------
+def initFileCheck():
+    try:
+        with open(pathing('credentials.json'), "r") as f:
+            login = json.load(f)
+            return [login["discord"]["owner_id"], login["discord"]["bot_token"]]
+    except FileNotFoundError:
+        errorMessage("The file login.json cannot be found.", sys.exc_info())
+        with open(pathing('credentials.json'), "r+") as f:
+            owner_id = input("Input discord user owner for owner_id: ")
+            bot_token = input("Input discord bot token for bot_token: ")
+            tokens = {
+            "discord": [owner_id, bot_token]
+            }
+            json.dump(logins, f)
+    except IndexError:
+        errorMessage("The file credentials.json has been modified and cannot be read.", sys.exc_info())
+#/////////////////////////////////////////////////////////////////////////////#
+
+
+#=============================================================================#
+#=============================================================================#
+# Running everything
+#=============================================================================#
+#=============================================================================#
+
+
+#-----------------------------------------------------------------------------#
+# Turning on the bot with some error checking
+###################################
+# discord.ext.commands as comms
 #-----------------------------------------------------------------------------
 class BotClient(comms.Bot):
     async def on_ready(self):
         print(f"Logging in as {bot.user}")
         print(f"{bot.user} ID: {bot.user.id}")
         print("Awaiting...")
-#/////////////////////////////////////////////////////////////////////////////#
 
-#-----------------------------------------------------------------------------#
-# Calling of bot functions
-#-----------------------------------------------------------------------------
 logins = initErrorCheck()
 try:
     bot = BotClient(command_prefix = "$", owner_id = logins[0])
